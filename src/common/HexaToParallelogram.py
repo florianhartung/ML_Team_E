9b3b66cd54d7e40f6dec81531d8e0439dddf987a
Bjarne <bkarsten@unitybox.de>
2025-03-14 19:48:28 +0100

    feat: add gpu support to HexaToParallelogram

--- src/common/HexaToParallelogram.py
+++ src/common/HexaToParallelogram.py
@@ -1,10 +1,11 @@
 import torch
+import torch.nn as nn
 from magicdl import magic
 from src.common import NUM_PIXELS
 
-class HexaToParallelogram(torch.nn.Module):
+class HexaToParallelogram(nn.Module):
     def __init__(self, padding_value=0.0):
-        super(HexaToParallelogram, self).__init__()
+        super().__init__()
         self._lt = magic.Geometry.lookup_table()
         self._padding_value = padding_value
 
@@ -13,7 +14,7 @@ class HexaToParallelogram(torch.nn.Module):
             hexa = hexa[:NUM_PIXELS]
         qs, rs = (list(t) for t in zip(*self._lt.keys()))
         q_max, r_max = max(qs), max(rs)
-        tensor = torch.full((2 * q_max + 1, 2 * r_max + 1), fill_value=self._padding_value)
+        tensor = torch.full((2 * q_max + 1, 2 * r_max + 1), fill_value=self._padding_value).to(hexa.get_device())
         for q in range(2*q_max+1):
             for r in range(2*r_max+1):
                 key = (q-q_max, r-r_max)

e667db1e56e4339bb5782f95b42e6c042262bdef
Florian Hartung <florian.hartung5+github@gmail.com>
2025-03-17 23:27:18 +0100

    fix: batchdataset for regression

--- src/infer_true_shower_parameters/cnn/cnn.py
+++ src/infer_true_shower_parameters/cnn/cnn.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from src.common.HexaToParallelogram import HexaToParallelogram
 import pandas as pd
-from src.common.batch import BatchDataset
 from torch.utils.data import DataLoader
 import torch.optim as optim
 import matplotlib.pyplot as plt
@@ -76,6 +75,26 @@ class ParticleCNNRegressor(nn.Module):
 
 def r_sq(pred, actual):
     return 1 - torch.sum((pred - actual) ** 2) / torch.sum((actual - torch.mean(actual)) ** 2)
+
+class BatchDataset(torch.utils.data.Dataset):
+    """
+    Usage:
+    >> from torch.utils.data import DataLoader
+    >> dataset = BatchDataset(X_train, y_train)
+    >> loader = DataLoader(dataset, batch_size=64) # This should be done in each epoch
+    >> for x, y in loader: # x and y are batches
+    >>      ...
+    """
+    def __init__(self, *datasets):
+        super(BatchDataset, self).__init__()
+        self.datasets = [*datasets]
+        assert all([dataset.shape[0] == datasets[0].shape[0] for dataset in datasets])
+
+    def __len__(self):
+        return self.datasets[0].shape[0]
+
+    def __getitem__(self, index):
+        return [dataset[index] for dataset in self.datasets]
 
 def train(
     train_data: pd.DataFrame,

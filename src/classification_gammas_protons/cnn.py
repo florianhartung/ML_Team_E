02f436bdf178e32b4e65306a64c67d4312c15792
Florian Hartung <florian.hartung5+github@gmail.com>
2025-03-17 16:36:48 +0100

    feat: other classification cnns and visualizations

--- src/classification_gammas_protons/cnn.py
+++ src/classification_gammas_protons/cnn.py
@@ -4,19 +4,26 @@ from torch import optim
 import torch.nn as nn
 import torch.nn.functional as F
 import pandas as pd
+import numpy as np
 import time
+import matplotlib.pyplot as plt
+from src.common import visualizations
 
 from src.common.HexaToParallelogram import HexaToParallelogram
+
+# Decrease this for less memory consumption at the cost of performance for evaluation
+VALIDATION_TEST_BATCH_SIZE = 1024
+
 
 
 class ParticleClassificationDataset(Dataset):
     def __init__(
         self,
-        df,
+        df: pd.DataFrame,
         image_features: list[list[str]],
-        additional_features,
-        class_feature,
-        device,
+        additional_features: list[str],
+        class_feature: str,
+        device: torch.device,
     ):
         self.images = [df[features].values for features in image_features]
         self.features = df[additional_features].values
@@ -28,13 +35,10 @@ class ParticleClassificationDataset(Dataset):
 
     def __getitem__(self, idx):
         images = torch.stack(
-            [
-                torch.tensor(image[idx], dtype=torch.float).to(self.device)
-                for image in self.images
-            ]
-        ).to(self.device)
-        features = torch.tensor(self.features[idx], dtype=torch.float).to(self.device)
-        label = torch.tensor(self.labels[idx], dtype=torch.float32).to(self.device)
+            [torch.tensor(image[idx], dtype=torch.float) for image in self.images]
+        )
+        features = torch.tensor(self.features[idx], dtype=torch.float)
+        label = torch.tensor(self.labels[idx], dtype=torch.float32)
         return images, features, label
 
 
@@ -46,6 +50,7 @@ class CNN(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
             nn.Conv2d(8, 16, 5),
+            nn.ReLU(),
             nn.MaxPool2d(2, 2),
         )
 
@@ -85,73 +90,121 @@ def train(
     additional_features: list[str],
     class_feature: str,
     device: torch.device,
+    pos_weight: float,
     epochs=10,
 ) -> ParticleCNNClassifier:
-    batch_size = 32
-
-    # Build torch datasets and dataloaders (this does not copy the data)
-    make_dataset = lambda data: ParticleClassificationDataset(
-        data, image_features, additional_features, class_feature, device
-    )
     train_loader = DataLoader(
-        make_dataset(train_data), batch_size=batch_size, shuffle=True
-    )
-    validation_loader = DataLoader(
-        make_dataset(validation_data), batch_size=batch_size, shuffle=False
+        ParticleClassificationDataset(
+            train_data, image_features, additional_features, class_feature, device
+        ),
+        batch_size=128,
+        shuffle=True,
     )
 
     model = ParticleCNNClassifier(len(additional_features))
     model.to(device)
 
-    criterion = nn.BCEWithLogitsLoss()
-    optimizer = optim.Adam(model.parameters(), lr=5e-4, weight_decay=1e-2)
+    training_loss_history = np.zeros(epochs)
+    validation_loss_history = np.zeros(epochs)
+    training_accuracy_history = np.zeros(epochs)
+    validation_accuracy_history = np.zeros(epochs)
+
+    optimizer = optim.AdamW(model.parameters(), lr=1e-3, weight_decay=1e-2)
+    loss_function = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([pos_weight]).to(device))
 
     for epoch in range(epochs):
-
         epoch_start = time.time()
         model.train()
-        epoch_accum_loss = 0
+        training_loss = 0
         correct, total = 0, 0
 
-        for images, additional_features, labels in train_loader:
+        for images_batch, additional_features_batch, labels_batch in train_loader:
             optimizer.zero_grad()
 
-            outputs = model(images, additional_features)
+            images_batch, additional_features_batch, labels_batch = (
+                images_batch.to(device),
+                additional_features_batch.to(device),
+                labels_batch.to(device),
+            )
 
-            loss = criterion(outputs, labels)
+            outputs = model(images_batch, additional_features_batch)
+
+            loss = loss_function(outputs, labels_batch)
             loss.backward()
             optimizer.step()
 
-            epoch_accum_loss += loss.item()
+            training_loss += loss.item()
             predicted = torch.round(torch.sigmoid(outputs))
-            correct += (predicted == labels).sum().item()
-            total += labels.size(0)
+            correct += (predicted == labels_batch).sum().item()
+            total += labels_batch.size(0)
 
-        epoch_accum_loss /= len(train_loader)
-        epoch_accuracy = correct / total
+        training_loss /= len(train_loader)
+        training_accuracy = correct / total
 
-        val_loss, val_acc = evaluate_model(model, validation_loader, criterion, device)
+        validation_loss, validation_accuracy = evaluate(
+            model,
+            validation_data,
+            image_features,
+            additional_features,
+            class_feature,
+            device,
+        )
+
+        training_loss_history[epoch] = training_loss
+        validation_loss_history[epoch] = validation_loss
+        training_accuracy_history[epoch] = training_accuracy
+        validation_accuracy_history[epoch] = validation_accuracy
 
         epoch_end = time.time()
-        print(
-            f"Epoch {epoch+1}/{epochs} - Train Loss: {epoch_accum_loss:.8f} Acc: {epoch_accuracy:.8f} | Val Loss: {val_loss:.8f} Val Acc: {val_acc:.8f} | Epoch took {epoch_end - epoch_start:.2f}s"
+        visualizations.print_training_progress(
+            epoch,
+            epochs,
+            epoch_end - epoch_start,
+            (training_loss, training_accuracy),
+            (validation_loss, validation_accuracy),
         )
+
+    visualizations.plot_training_validation(
+        training_loss_history,
+        training_accuracy_history,
+        validation_loss_history,
+        validation_accuracy_history,
+    )
 
     return model
 
 
-def evaluate_model(
-    model: ParticleCNNClassifier, dataloader, criterion, device
+def evaluate(
+    model: ParticleCNNClassifier,
+    data: pd.DataFrame,
+    image_features: list[list[str]],
+    additional_features: list[str],
+    class_feature: str,
+    device: torch.device,
 ) -> (float, float):
+    dataloader = DataLoader(
+        ParticleClassificationDataset(
+            data, image_features, additional_features, class_feature, device
+        ),
+        batch_size=VALIDATION_TEST_BATCH_SIZE,
+    )
+
     model.eval()
     loss = 0
     correct, total = 0, 0
 
+    loss_function = nn.BCEWithLogitsLoss()
+
     with torch.no_grad():
         for images, additional_features, labels in dataloader:
+            images, additional_features, labels = (
+                images.to(device),
+                additional_features.to(device),
+                labels.to(device),
+            )
 
             outputs = model(images, additional_features)
-            loss += criterion(outputs, labels).item()
+            loss += loss_function(outputs, labels).item()
             predicted = torch.round(torch.sigmoid(outputs))
             correct += (predicted == labels).sum().item()
             total += labels.size(0)
@@ -162,7 +215,9 @@ def evaluate_model(
 def save(model: ParticleCNNClassifier, path):
     torch.save(model.state_dict(), path)
 
-def load(path, additional_features):
+
+def load(path, additional_features, device):
     model = ParticleCNNClassifier(len(additional_features))
     model.load_state_dict(torch.load(path))
+    model.to(device)
     return model
---
+++ src/common/visualizations.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from typing import Optional
+
+
+def plot_training_validation(
+    training_losses: np.ndarray,
+    training_accuracies: np.ndarray,
+    validation_losses: np.ndarray,
+    validation_accuracies: np.ndarray,
+):
+    fig, ax = plt.subplots(nrows=1, ncols=2)
+    fig.set_figwidth(15)
+
+    epochs = range(training_losses.shape[0])
+
+    ax[0].set_title("Loss")
+    ax[0].plot(epochs, training_losses, label="Training Loss")
+    ax[0].plot(epochs, validation_losses, label="Validation Loss")
+
+    ax[1].set_title("Accuracy")
+    ax[1].plot(epochs, training_accuracies, label="Training Accuracy")
+    ax[1].plot(epochs, validation_accuracies, label="Validation Accuracy")
+
+
+def print_training_progress(
+    current_epoch: int,
+    max_epochs: Optional[int],
+    epoch_time_seconds: Optional[int],
+    train_loss_accuracy: (float, float),
+    validation_loss_accuracy: (float, float),
+):
+    epoch_msg = f"Epoch {current_epoch + 1}/{str(max_epochs) or "?"}"
+    time_msg = f" (took {epoch_time_seconds:.1f}s)" if epoch_time_seconds is not None else " "
+    train_msg = f"TRAIN(loss|acc): {train_loss_accuracy[0]:.2f}|{100 * train_loss_accuracy[1]:.2f}%"
+    validation_msg = f"VALID(loss|acc): {validation_loss_accuracy[0]:.2f}|{100 * validation_loss_accuracy[1]:.2f}%"
+
+    print(f"{epoch_msg}{time_msg} - {train_msg} - {validation_msg}")
+
+def print_test_results(loss: float, accuracy: float):
+    print(f"Test data evaluation - LOSS: {loss:.2f} - ACC: {100 * accuracy:.3f}%")
